32eb4539a001f0ec9ff95c5df7bcd16b71d221fa
Bisma-Rasheed <bismarasheed95@gmail.com>
2023-01-14 10:59:20 +0500

    some file changes

--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import {excerciseSlice} from './excercisereducer'
+import { excerciseSlice } from './excercisereducer'
 
 export const store = configureStore({
   reducer: {
-    exce:excerciseSlice.reducer
+    exce: excerciseSlice.reducer
   },
 })
